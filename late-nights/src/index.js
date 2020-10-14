5d43d16c96511fceb7354bee4bd557436b239ccc
Tristan Elliott <tristanelliott7@gmail.com>
2020-10-14 16:40:11 -0300

    adding the global css resets

--- late-nights/src/index.js
+++ late-nights/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom'
 import App from './Components/App'
+import GlobalStyle from './Styling/Global'
 
 
 
 ReactDOM.render(
   <Router>
+    <GlobalStyle/>
     <App />
   </Router>,
   document.getElementById('root'))
